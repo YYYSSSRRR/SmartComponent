c264b4560c16eff0c0a3e9bf03b5e46607ef513a
YYYSSSRRR <2163368320@qq.com>
2025-08-04 11:57:07 +0800

    feat:注册组件与前后端通信

--- backend/llmConstant.py
+++ backend/llmConstant.py
@@ -6,8 +6,8 @@ import openai
 from backend.utils.llmService import LLMService
 
 load_dotenv()
-openai.api_key=os.getenv("OPENAI_API_KEY")
-os.environ["OPENAI_API_KEY"]="sk-IGmFA9jyH2mlwcpelXbarOuipa3I55tF4We1fFYiYCElBXhS"
+api_key=os.getenv("OPENAI_API_KEY")
+os.environ["OPENAI_API_KEY"]=api_key
 os.environ["OPENAI_API_BASE"]="https://api.chatanywhere.tech/v1"
 from langchain.chat_models import init_chat_model
 llm=init_chat_model("gpt-4o-mini",model_provider="openai")
